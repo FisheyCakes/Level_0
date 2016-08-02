9e6bc7b7e8c32f6e9dc1eadc1472cfb32fa5381f
LEAUGE COMPUTER <DO NOT CHANGE THIS/>
2016-08-01 18:28:30 -0700

    Level_0_Day_1

---
+++ src/KnockKnock_Joke.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class KnockKnock_Joke {
+	public static void main(String[] args) {
+		JOptionPane.showInputDialog("Why did Suzie fall off the swing?");
+		JOptionPane.showMessageDialog(null, "Because she didn't have any arms.");
+		JOptionPane.showInputDialog("Knock Knock");
+		JOptionPane.showMessageDialog(null, "Not Suzie");
+	}
+
+}
---
+++ src/Zambies.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane;
+
+public class Zambies {
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "You must wake up and eat cereal or waffles for breakfest(NOT brains)");
+		JOptionPane.showMessageDialog(null, "Then, you must get dressed");
+		JOptionPane.showMessageDialog(null, "Afterwards, you have to brush your teeth for 2 minuets");
+		JOptionPane.showConfirmDialog(null,
+				"After this, you must go out to the job of the person you are impersonating");
+		JOptionPane.showMessageDialog(null, "If someone says Hi, Hello, or Hey, you must respond with hello");
+		JOptionPane.showMessageDialog(null,
+				"If the small metal/plastic square in your pocket starts vibrating and making sound, you must push the green button, hold it up to your ear, and engage in a normal human conversation with the voice coming out of it");
+		JOptionPane.showMessageDialog(null, "When you arrive at 'Your' job, you must excecute your duties there");
+		JOptionPane.showMessageDialog(null, "At lunch, you must eat a normal human lunch, possibly one that is provided by 'Your' company. Remember, if you go to a resturant, you need to pay with money");
+
+	}
+}
