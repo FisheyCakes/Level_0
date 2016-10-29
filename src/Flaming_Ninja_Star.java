d05854026e86b1ac4bb0d8951113104fb9ce265d
rylr <league@WTS9.attlocal.net>
2016-10-29 11:02:35 -0700

    MAKE_UP_DAY

--- src/Flaming_Ninja_Star.java
+++ src/Flaming_Ninja_Star.java
@@ -1,46 +1,43 @@
 import org.jointheleague.graphical.robot.Robot;
-import java.awt.Color;
-
-
-
 
 /*** Teacher's note ***/
- /* Before beginning recipe: 
- * 1. ask students to find and explain the variables in this recipe. 
- * 2. ask students what robot commands they think they will use to make picture in the laminated hand-outs: http://bit.ly/ninja-star */
-
+/*
+ * Before beginning recipe: 1. ask students to find and explain the variables in
+ * this recipe. 2. ask students what robot commands they think they will use to
+ * make picture in the laminated hand-outs: http://bit.ly/ninja-star
+ */
 
 public class Flaming_Ninja_Star {
 	public static void main(String[] args) {
 
-
-		int baseSize = 300;		//the size of the black part of the star
-		int flameSize = 200;		//the length of the flaming arms
-
+		int baseSize = 300; // the size of the black part of the star
+		int flameSize = 200; // the length of the flaming arms
 
 		// 1. Make a new robot, and set it's pen down.
-Robot Steve = new Robot("vic");
-Steve.penDown();
+		Robot Steve = new Robot("vic");
+		Steve.penDown();
 
-		// *14. Use the methods setX and setY to move the ninja star into the center of the screen
-		
-		// *15. Make some adjustments to see what other kinds of shapes you can make.
-
+		// *14. Use the methods setX and setY to move the ninja star into the
+		// center of the screen
+		Steve.setY(400);
+		Steve.setX(900);
+		// *15. Make some adjustments to see what other kinds of shapes you can
+		// make.
 
 		// 12. Set the robot speed to 10
-Steve.setSpeed(100);
+		Steve.setSpeed(100);
 
 		// 13. Make all the code below repeat 25 times
-for (int i = 0; i < 23;){
+		for (int i = 0; i < 25; i++) {
 
 			// 2. Turn the robot 1/8 of a circle
-		Steve.turn(45);
+			Steve.turn(45);
 			// 3. Move the robot 64 pixels
-		Steve.move(64);
-		Steve.setPenColor(255, 136, 0);
+			Steve.move(64);
+			Steve.setPenColor(255, 136, 0);
 
-			
-			// 4. Turn the robot 40 degrees to the LEFT. (Negative numbers will turn the robot counter-clockwise.)
+			// 4. Turn the robot 40 degrees to the LEFT. (Negative numbers will
+			// turn the robot counter-clockwise.)
 			Steve.turn(-40);
 			// 5. Move the robot the distance in the variable flameSize
 			Steve.move(flameSize);
@@ -53,19 +50,13 @@ for (int i = 0; i < 23;){
 			// 9. Move the robot the distance in the variable baseSize
 			Steve.setPenColor(0, 0, 0);
 			Steve.move(baseSize);
-			
-			// 10. Check that your shape is the same as Figure 1. This is one arm of the ninja star.
+
+			// 10. Check that your shape is the same as Figure 1. This is one
+			// arm of the ninja star.
 			// 11. Color your ninja star like Figure 2.
-		
+
+		}
+
 	}
 
-
 }
-
-}
-
-
-
-
-
-
--- src/ForLoopGauntletOfInfinity.java
+++ src/ForLoopGauntletOfInfinity.java
@@ -1,25 +1,21 @@
 
 public class ForLoopGauntletOfInfinity {
-public static void main(String[] args) {
-	/*for (int i = 0; i <= 100; i++){
-		System.out.println(i);
+	public static void main(String[] args) {
+		/*
+		 * for (int i = 0; i <= 100; i++) { System.out.println(i); } for (int i
+		 * = 100; i >= 0; i--) { System.out.println(i); } for (int i = 2; i <=
+		 * 100; i = i + 2) { System.out.println(i); } for (int i = 1; i <= 99; i
+		 * = i + 2) { System.out.println(i); } for (int i = 0; i <= 500; i++) {
+		 * if (i % 2 == 1) { System.out.println(i + " Odd"); } else {
+		 * System.out.println(i + " Even"); } }
+		 * 
+		 * for (int i = 7; i <= 777; i = i + 7) { System.out.println(i);
+		 * 
+		 * }
+		 * 
+		 * for (int i = 2003; i <= 2016; i++) { System.out.print("In " + i);
+		 * String Ducks = Integer.toString(i - 2003); System.out.println(
+		 * " I was " + Ducks); }
+		 */
 	}
-	for (int i = 100; i >= 0; i--){
-		System.out.println(i);
-	}
-	for (int i = 2; i <= 100; i=i+2){
-		System.out.println(i);
-	}
-	for(int i =1; i <= 99; i=i+2){
-		System.out.println(i);
-	}*/
-	for (int i = 0; i <=500; i++){
-		if(i % 2==1){
-			System.out.println(i +" Odd");
-		}
-		else{
-			System.out.println(i +" Even");
-		}
-	}
-}
 }
